30e9ec66d0c623a3e2f0b7310f66a31de1fe9c33
Amaralzaao <72256470+Amaralzaao@users.noreply.github.com>
2021-12-03 21:08:55 -0300

    Criando e ajustando arquivos do front

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ProjectRoutes from './routes';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <ProjectRoutes/>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
